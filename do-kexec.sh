79faf2c8f7d9d3b24b9a05a66f5edef688f5bc36
Rafał Janiczek <rafal.janiczek@gmail.com>
2003-07-13 15:12:41 +0000

    - script for kexec
    Changed files:
        do-kexec.sh -> 1.1

---
+++ do-kexec.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+KEXEC=/usr/sbin/kexec
+usage()
+{
+	echo "Usage: do-kexec.sh /boot/bzImage [commandline options]"
+	exit 1
+}
+
+if [ $# -lt 1 ]
+then
+	usage
+fi
+
+IMAGE=$1
+shift
+$KEXEC -l $IMAGE --command-line="$(cat /proc/cmdline) $*"
+$KEXEC -e --command-line="$(cat /proc/cmdline) $*"
